402b25bebbf2c8f349151c667248684e93128deb
Ziad Saab <ziad.saab@gmail.com>
2023-05-17 14:51:32 -0400

    cleanup

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -40,8 +40,6 @@ const makeRequestWithApiKey = async (apiKey: string) => {
  * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
  *
  * @param args - The request handler args as object.
- * @param args.origin - The origin of the request, e.g., the website that
- * invoked the snap.
  * @param args.request - A validated JSON-RPC request object.
  * @returns The result of `snap_dialog`.
  * @throws If the request method is not valid for this snap.
@@ -50,9 +48,10 @@ export const onRpcRequest: OnRpcRequestHandler = async ({ request }) => {
   switch (request.method) {
     case 'remove_api_key':
       await setApiKey(null);
-      break;
+      return true;
     case 'set_api_key':
       if (
+        request.params &&
         'apiKey' in request.params &&
         typeof request.params.apiKey === 'string'
       ) {
